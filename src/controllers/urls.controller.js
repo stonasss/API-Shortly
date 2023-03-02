e8ba42a1213d2a1fd4c60db017c3b86491d2f36b
stonasss <luis.b.santos96@gmail.com>
2023-03-02 00:42:49 -0300

    feat: open shortUrl

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -16,13 +16,10 @@ export async function shortUrl(req, res) {
             VALUES ($1, $2, $3)`,
             [id, url, shorterUrl]
         );
-        return res
-            .status(201)
-            .json({
-                id: links.rows[0].id,
-                shortUrl: links.rows[0].shortUrl,
-            });
-
+        return res.status(201).json({
+            id: links.rows[0].id,
+            shortUrl: links.rows[0].shortUrl,
+        });
     } catch (err) {
         return res.status(500).send(err.message);
     }
@@ -37,15 +34,43 @@ export async function getUrl(req, res) {
             SELECT * FROM "public.urls" WHERE "id" = $1`,
             [id]
         );
-        if (urlExists.rows.length === 0) return res.status(404).send("Id has no urls");
+        if (urlExists.rows.length === 0)
+            return res.status(404).send("Id has no urls");
 
-        return res
-            .status(200)
-            .json({ 
-                id: urlExists.rows[0].id,
-                shortUrl: urlExists.rows[0].shortUrl,
-                url: urlExists.rows[0].url,
-            });
+        return res.status(200).json({
+            id: urlExists.rows[0].id,
+            shortUrl: urlExists.rows[0].shortUrl,
+            url: urlExists.rows[0].url,
+        });
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function openUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const urlExists = await db.query(
+            `
+            SELECT * FROM "public.urls"
+            WHERE "shortURL" = $1`,
+            [shortUrl]
+        );
+        if (urlExists.rows.length === 0)
+            return res.status(404).send("Invalid url");
+
+        const addCount = urlExists.rows[0].visitCount + 1;
+        await db.query(
+            `
+            UPDATE "public.urls"
+            SET "visitCount" = $1
+            WHERE "shortUrl" = $2`,
+            [addCount, shortUrl]
+        );
+
+        const selectedUrl = urlExists.rows[0].url;
+        return res.redirect(selectedUrl);
     } catch (err) {
         return res.status(500).send(err.message);
     }
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortUrl } from "../controllers/urls.controller.js";
+import { getUrl, openUrl, shortUrl } from "../controllers/urls.controller.js";
 import { validateToken } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { urlSchema } from "../schemas/urlSchema.js";
@@ -7,5 +7,7 @@ import { urlSchema } from "../schemas/urlSchema.js";
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", validateToken, validateSchema(urlSchema), shortUrl);
+urlsRouter.get("/urls/:id", getUrl);
+urlsRouter.get("/urls/open/:shortUrl", openUrl);
 
 export default urlsRouter;
